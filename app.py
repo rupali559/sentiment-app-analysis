0eaa8dafb69497fcec6b609426543321d3de81ad
rupali559 <rupalivaje99@gmail.com>
2025-08-19 22:19:54 -0400

    Added app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from textblob import TextBlob
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Sentiment Analysis API is running. Use POST /analyze"
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    text = data.get('text', '')
+    print(f"Received text: {text}")  # Debug
+
+    polarity = TextBlob(text).sentiment.polarity
+    print(f"Polarity: {polarity}")  # Debug
+    if polarity > 0.1:
+        sentiment = "Positive"
+    elif polarity < -0.1:
+        sentiment = "Negative"
+    else:
+        sentiment = "Neutral"
+
+    return jsonify({"sentiment": sentiment})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+
